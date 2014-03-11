624d6675ca824496ec19da0a8f7db1735cefac36
Dave Elliott <david.elliott@sage.com>
2014-03-11 10:41:11 +0000

    Added rspec defaults
    updated install script

--- install.sh
+++ install.sh
@@ -8,24 +8,28 @@
 
 dir=$PWD                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim tmux.conf gitconfig gitignore gemrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim tmux.conf gitconfig gitignore gemrc rspec"    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo -ne "Creating $olddir for backup of any existing dotfiles in ~/"
 mkdir -p $olddir
-echo "...done"
+echo " ...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo -ne "Changing to $dir directory"
 cd $dir
-echo "...done"
+echo " ...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo -ne "Moving ~/.$file to $olddir/$file"
+  mv ~/.$file ~/dotfiles_old/
+  echo " ...done"
+
+  echo -ne "Creating symlink to $file in home directory"
+  ln -s $dir/$file ~/.$file
+  echo " ...done"
+  echo
 done
