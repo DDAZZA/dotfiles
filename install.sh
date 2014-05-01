462532d5685045d8ac072be97d1c79bc053220ed
Dave Elliott <ddazza@gmail.com>
2014-05-01 19:58:40 +0100

    Fix install script
    Changed git-prompt, git-completion source

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 set -o nounset
-set -o errexit
-
 readonly DIR=$PWD                                                               # dotfiles directory
 readonly BACKUP_DIR=~/dotfiles_old/$(date '+%Y-%m-%d')                              # old dotfiles backup directory
 readonly FILES="bashrc vimrc vim tmux.conf gitconfig gitignore gemrc rspec"     # list of files/folders to symlink in homedir
@@ -33,12 +31,12 @@ done
 
 if [[ ! -f ~/.git-completion.sh ]]; then
   echo 'Installing git completion'
-  curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.sh
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.sh
 fi
 
 if [[ ! -f ~/.git-prompt.sh ]]; then
   echo 'Installing git prompt'
-  curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 fi
 
 source ~/.bashrc
