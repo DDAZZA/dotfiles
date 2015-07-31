3ac48fe3b196355e3b6d283c894d9430c7c65fd2
Dave Elliott <david.elliott@sage.com>
2015-07-31 11:41:06 +0100

    Updated install script to work with vundle

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 # .install.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
+command -v wget >/dev/null 2>&1 || { echo >&2 "wget not installed.  Aborting."; exit 1; }
+command -v tmux >/dev/null 2>&1 || { echo >&2 "tmux not installed.  Aborting."; exit 1; }
+command -v vim >/dev/null 2>&1 || { echo >&2 "vim not installed.  Aborting."; exit 1; }
+
 set -o nounset
 readonly DIR=$PWD                                                               # dotfiles directory
 readonly BACKUP_DIR=~/dotfiles_old/$(date '+%Y%m%d_%H%M%S')                      # old dotfiles backup directory
@@ -33,4 +37,5 @@ if [ ! -f ~/.git-prompt.sh ]; then
   wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 fi
 
+vim +PluginInstall +qall
 source ~/.bashrc
