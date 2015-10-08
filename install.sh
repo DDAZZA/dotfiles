ca087669ca6f7b4e7dfd88579e9e9ca5818b5f4c
Dave Elliott <DDAZZA@users.noreply.github.com>
2015-10-08 20:36:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 ############################
 
 set -o nounset
-readonly DIR=$PWD                                                               # dotfiles directory
+readonly DIR=~/.dotfiles                                                             # dotfiles directory
 readonly BACKUP_DIR=~/dotfiles_old/$(date '+%Y%m%d_%H%M%S')                      # old dotfiles backup directory
-readonly FILES="bashrc vimrc vim tmux.conf gitconfig gitignore gemrc rspec"     # list of files/folders to symlink in homedir
+readonly FILES="bash_profile bashrc vimrc vim tmux.conf gitconfig gitignore gemrc rspec"     # list of files/folders to symlink in homedir
 
 # change to the dotfiles directory
 cd $DIR
@@ -33,3 +33,5 @@ fi
 if [ ! -f ~/.git-prompt.sh ]; then
   wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 fi
+
+echo "Run 'source ~/.bash_profile'"
